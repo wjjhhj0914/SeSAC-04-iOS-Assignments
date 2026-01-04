0f4726c81f9bd87ea1142d8736b58b0067c52e13
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-05 00:58:54 +0900

    feat: Day 03 EmotionDiary 과제 구현

---
+++ Day03_Assignment/EmotionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmotionViewController.swift
+//  Day03_Assignment
+//
+//  Created by Hyojung Jang on 1/4/26.
+//
+
+import UIKit
+
+class EmotionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
