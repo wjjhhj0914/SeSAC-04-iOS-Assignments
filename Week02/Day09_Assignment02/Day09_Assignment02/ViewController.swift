0e5ed9db531f0a83371617d29521b24194f5d022
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-11 14:06:45 +0900

    add: Day 09 두 번째 과제 프로젝트 생성

---
+++ Week02/Day09_Assignment02/Day09_Assignment02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day09_Assignment02
+//
+//  Created by Hyojung Jang on 1/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
