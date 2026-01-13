91229191d238c8c38a8cabd0c96b5deb30508671
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-13 19:38:41 +0900

    design: 커스텀 셀 내부에서 버튼과 레이블 레이아웃 설정

---
+++ Week03/Day11_Assignment/Day11_Assignment/TravelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelTableViewCell.swift
+//  Day11_Assignment
+//
+//  Created by Hyojung Jang on 1/13/26.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
