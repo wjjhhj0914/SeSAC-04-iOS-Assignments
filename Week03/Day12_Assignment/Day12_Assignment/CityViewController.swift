a3c58cb7a2eb200e663aa49c0d870409ee2cfc8d
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-14 16:31:56 +0900

    design: 컬렉션뷰 과제 레이아웃 구성

---
+++ Week03/Day12_Assignment/Day12_Assignment/CityViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CityViewController.swift
+//  Day12_Assignment
+//
+//  Created by Hyojung Jang on 1/14/26.
+//
+
+import UIKit
+
+class CityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
