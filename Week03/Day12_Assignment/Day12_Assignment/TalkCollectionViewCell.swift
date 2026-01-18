c98847f304a03e109145a071086707dc89446e14
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-18 14:34:02 +0900

    feat: 채팅 목록 화면 구현

---
+++ Week03/Day12_Assignment/Day12_Assignment/TalkCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TalkCollectionViewCell.swift
+//  Day12_Assignment
+//
+//  Created by Hyojung Jang on 1/18/26.
+//
+
+import UIKit
+
+class TalkCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
