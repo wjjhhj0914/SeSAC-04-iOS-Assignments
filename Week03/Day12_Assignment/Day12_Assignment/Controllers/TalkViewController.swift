ca6ad4def0cb1d4a80cbb7a67fababd17e350ec3
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-20 08:33:04 +0900

    feat: 15일차 과제 - SearchBar 채팅방 목록 화면 구현

--- Week03/Day12_Assignment/Day12_Assignment/Controllers/TalkViewController.swift
+++ Week03/Day12_Assignment/Day12_Assignment/Controllers/TalkViewController.swift
@@ -22,6 +22,8 @@ class TalkViewController: UIViewController {
         chatListCollectionView.delegate = self
         chatListCollectionView.dataSource = self
         
+        mainSearchBar.delegate = self
+        
         let xib = UINib(nibName: TalkCollectionViewCell.identifier, bundle: nil)
         chatListCollectionView.register(xib, forCellWithReuseIdentifier: TalkCollectionViewCell.identifier)
         
@@ -70,5 +72,48 @@ extension TalkViewController: UICollectionViewDelegate, UICollectionViewDataSour
         vc.chatData = selectedChat
         
         navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension TalkViewController: UISearchBarDelegate {
+    // 키보드의 검색 버튼을 눌렀을 때 실행되는 함수
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        if let text = searchBar.text?.trimmingCharacters(in: .whitespaces), !text.isEmpty {
+            // 검색어가 있는 경우
+            // 1단계: 검색된 결과물 담을 빈 바구니 생성
+            var filteredResult: [ChatRoom] = []
+            
+            // 2단계: 전체 데이터를 하나씩 꺼내서 확인
+            for chatRoom in mockChatRooms {
+                var isNameMatched = false
+                
+                // 3단계: id를 꺼내서 비교
+                for participantId in chatRoom.participantIds {
+                    // 4단계: 전체 유저 목록에서 해당 id를 가진 유저를 찾는다
+                    for user in mockUsers {
+                        if user.userId == participantId {
+                            // 5단계: 그 id를 가진 유저가 검색어에 포함되어 있는지 확인
+                            if user.userName.contains(text.lowercased()) {
+                                isNameMatched = true
+                            }
+                        }
+                    }
+                }
+                
+                // 6단계: 있으면 바구니에 담음
+                if isNameMatched {
+                    filteredResult.append(chatRoom)
+                }
+            }
+            // 7단계: 바구니 교체
+            chatList = filteredResult
+        } else {
+            // 공란일 경우 전체 친구 목록 보여주기
+            chatList = mockChatRooms
+        }
+        
+        chatListCollectionView.reloadData()
+        searchBar.resignFirstResponder()
     }
 }
