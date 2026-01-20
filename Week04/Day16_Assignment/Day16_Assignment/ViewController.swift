01622961d86aebed23578585212d1459110947a8
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-20 16:10:07 +0900

    add: 16일차 과제 프로젝트 생성

---
+++ Week04/Day16_Assignment/Day16_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day16_Assignment
+//
+//  Created by Hyojung Jang on 1/20/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
