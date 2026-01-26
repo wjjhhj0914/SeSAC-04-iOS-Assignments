9d80fbc775d37c5a9de84d36f93278b1fcb209a8
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-26 23:55:04 +0900

    chore: SnapKit 라이브러리 추가

---
+++ Week05/Day20_Assignment/Day20_Assignment/Base/BaseViewController.swift
@@ -0,0 +1,8 @@
+//
+//  BaseViewController.swift
+//  Day20_Assignment
+//
+//  Created by Hyojung Jang on 1/26/26.
+//
+
+import Foundation
---
+++ Week05/Day20_Assignment/Day20_Assignment/SearchPhoto/SearchPhotoViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SearchPhotoViewController.swift
+//  Day20_Assignment
+//
+//  Created by Hyojung Jang on 1/26/26.
+//
+
+import Foundation
