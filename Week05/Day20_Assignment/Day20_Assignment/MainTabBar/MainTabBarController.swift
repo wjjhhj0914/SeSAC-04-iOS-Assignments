7fc93e6b161607eabaf8877ffb0bd468b6a86ef7
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-27 00:19:33 +0900

    feat: 탭바 구조 설계 및 화면 연결

---
+++ Week05/Day20_Assignment/Day20_Assignment/MainTabBar/MainTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  MainTabBarController.swift
+//  Day20_Assignment
+//
+//  Created by Hyojung Jang on 1/27/26.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTabs()
+    }
+    
+    private func setupTabs() {
+        let topicVC = UINavigationController(rootViewController: TopicViewController())
+        topicVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "photo"), tag: 0)
+        
+        let videoVC = UINavigationController(rootViewController: UIViewController())
+        videoVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "play.rectangle"), tag: 1)
+        
+        let searchVC = UINavigationController(rootViewController: SearchPhotoViewController())
+        searchVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "magnifyingglass"), tag: 2)
+        
+        let likeVC = UINavigationController(rootViewController: UIViewController())
+        likeVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "heart"), tag: 3)
+        
+        setViewControllers([topicVC, videoVC, searchVC, likeVC], animated: true)
+    }
+
+}
