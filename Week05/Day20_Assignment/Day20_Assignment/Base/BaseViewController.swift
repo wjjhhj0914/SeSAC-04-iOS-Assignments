a61ab0c51cf253c9438d3368b25d05f33a8f36aa
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-26 23:57:34 +0900

    chore: BaseViewController 클래스 추가

--- Week05/Day20_Assignment/Day20_Assignment/Base/BaseViewController.swift
+++ Week05/Day20_Assignment/Day20_Assignment/Base/BaseViewController.swift
@@ -5,4 +5,21 @@
 //  Created by Hyojung Jang on 1/26/26.
 //
 
-import Foundation
+import UIKit
+
+class BaseViewController: ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() {}
+    
+    func configureLayout() {}
+    
+    func configureView() {}
+}
