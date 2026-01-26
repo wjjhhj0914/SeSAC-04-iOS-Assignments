b02f39977aed06cbbcd4082b9de51694ba8007de
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-27 00:20:50 +0900

    feat: SearchPhotoViewController에 서치바 추가

--- Week05/Day20_Assignment/Day20_Assignment/SearchPhoto/SearchPhotoViewController.swift
+++ Week05/Day20_Assignment/Day20_Assignment/SearchPhoto/SearchPhotoViewController.swift
@@ -5,4 +5,27 @@
 //  Created by Hyojung Jang on 1/26/26.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class SearchPhotoViewController: BaseViewController {
+    let searchBar = UISearchBar()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(searchBar)
+    }
+    
+    override func configureLayout() {
+        searchBar.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func configureView() {
+        searchBar.placeholder = "사진을 검색해 보세요"
+    }
+}
