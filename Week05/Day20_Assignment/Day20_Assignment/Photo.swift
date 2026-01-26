9f154af822f85fcdcaf989f3834e552bb4265834
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-26 23:40:38 +0900

    feat: Unsplash Photo Search API 통신

--- Week05/Day20_Assignment/Day20_Assignment/Photo.swift
+++ Week05/Day20_Assignment/Day20_Assignment/Photo.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+// 사진 검색 결과 전체를 담는 식판
+struct PhotoSearchResponse: Decodable {
+    let total: Int
+    let total_pages: Int
+    let results: [Photo]
+}
+
+// 사진 한 장 식판
+struct Photo: Decodable {
+    let id: String
+    let created_at: String
+    let width: Int
+    let height: Int
+    let urls: PhotoUrl
+    let likes: Int
+    let user: PhotoUser
+}
+
+// 사진 이미지 링크
+struct PhotoUrl: Decodable {
+    let thumb: String
+    let small: String
+    let full: String
+}
+
+// 사진 작가
+struct PhotoUser: Decodable {
+    let name: String
+}
--- Week05/Day20_Assignment/Day20_Assignment/ViewController.swift
+++ Week05/Day20_Assignment/Day20_Assignment/ViewController.swift
@@ -6,12 +6,40 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        callRequest(query: "flower")
+    }
+    
+    func callRequest(query: String) {
+        let url = "https://api.unsplash.com/search/photos"
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Client-ID \(APIKey.UNSPLASH_ACCESS)"
+        ]
+        
+        let parameters: Parameters = [
+            "query": query,
+            "page": 1,
+            "per_page": 20
+        ]
+        
+        AF.request(url, method: .get, parameters: parameters, headers: headers)
+            .responseDecodable(of: PhotoSearchResponse.self) { response in
+                switch response.result {
+                case .success(let value):
+                    print(">>> 검색 결과 성공 >>>")
+                    print("전체 사진 개수: \(value.total)")
+                
+                case .failure(let error):
+                    print(">>> 에러 발생 >>> 에러: \(error)")
+                }
+            }
     }
 
 
