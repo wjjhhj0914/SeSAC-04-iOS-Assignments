ca562af8bc289f6aa3f129f7a50f9e752d150790
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-27 21:09:42 +0900

    feat: 네트워크 통신 로직 이동 및 사진 검색 결과 화면 표시
    - ViewController에 있던 API 연동 코드를 SearchPhotoViewController로 이동
    - CollectionView를 통해 사진 검색 결과 연동

--- Week05/Day20_Assignment/Day20_Assignment/SearchPhoto/SearchPhotoViewController.swift
+++ Week05/Day20_Assignment/Day20_Assignment/SearchPhoto/SearchPhotoViewController.swift
@@ -7,30 +7,61 @@
 
 import UIKit
 import SnapKit
+import Alamofire
+import Kingfisher
 
 class SearchPhotoViewController: BaseViewController {
-    let searchBar = UISearchBar()
+    let mainView = SearchPhotoView()
+    
+    var photoList: [Photo] = []
+    var startPage = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         navigationItem.title = "SEARCH PHOTO"
     }
     
     override func configureHierarchy() {
-        view.addSubview(searchBar)
+        view.addSubview(mainView)
     }
     
     override func configureLayout() {
-        searchBar.snp.makeConstraints { make in
-            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        mainView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
     }
     
     override func configureView() {
-        searchBar.placeholder = "키워드 검색"
-        searchBar.searchBarStyle = .minimal
-        searchBar.delegate = self
+        mainView.searchBar.delegate = self
+        mainView.collectionView.delegate = self
+        mainView.collectionView.dataSource = self
+    }
+    
+    // 네트워크 통신
+    func callRequest(query: String) {
+        let url = "https://api.unsplash.com/search/photos"
+        let headers: HTTPHeaders = [
+            "Authorization": "Client-ID \(APIKey.UNSPLASH_ACCESS)"
+        ]
+        let parameters: Parameters = [
+            "query": query,
+            "page": 1,
+            "per_page": 20
+        ]
+        
+        AF.request(url, method: .get, parameters: parameters, headers: headers)
+            .responseDecodable(of: PhotoSearchResponse.self) { response in
+                switch response.result {
+                case .success(let value):
+                    print(">>> 검색 결과 성공 >>>")
+                    print("전체 사진 개수: \(value.total)")
+                    self.photoList = value.results
+                    self.mainView.collectionView.reloadData()
+                
+                case .failure(let error):
+                    print(">>> 에러 발생 >>> 에러: \(error)")
+                }
+            }
     }
 }
 
@@ -41,7 +72,29 @@ extension SearchPhotoViewController: UISearchBarDelegate {
             showAlert(title: "알림", message: "검색어를 입력해 주세요!")
             return
         }
+        // 검색 시 페이지 1로 다시 초기화 및 네트워크 통신
+        startPage = 1
         callRequest(query: text)
         view.endEditing(true)
     }
+}
+
+// CollectionView
+extension SearchPhotoViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photoList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchResultCollectionViewCell.identifier, for: indexPath) as! SearchResultCollectionViewCell
+        
+        let data = photoList[indexPath.row]
+        if let url = URL(string: data.urls.thumb) {
+            cell.photoImageView.kf.setImage(with: url)
+        }
+        
+        return cell
+    }
+    
+    
 }
--- Week05/Day20_Assignment/Day20_Assignment/ViewController.swift
+++ Week05/Day20_Assignment/Day20_Assignment/ViewController.swift
@@ -14,33 +14,4 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-    
-    func callRequest(query: String) {
-        let url = "https://api.unsplash.com/search/photos"
-        
-        let headers: HTTPHeaders = [
-            "Authorization": "Client-ID \(APIKey.UNSPLASH_ACCESS)"
-        ]
-        
-        let parameters: Parameters = [
-            "query": query,
-            "page": 1,
-            "per_page": 20
-        ]
-        
-        AF.request(url, method: .get, parameters: parameters, headers: headers)
-            .responseDecodable(of: PhotoSearchResponse.self) { response in
-                switch response.result {
-                case .success(let value):
-                    print(">>> 검색 결과 성공 >>>")
-                    print("전체 사진 개수: \(value.total)")
-                
-                case .failure(let error):
-                    print(">>> 에러 발생 >>> 에러: \(error)")
-                }
-            }
-    }
-
-
 }
-
