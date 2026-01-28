fbf4598733aa34618dc6a548148d73a1f57c56e5
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-01-28 21:55:20 +0900

    refactor: 네트워크 통신 Singleton Pattern 적용 및 통계 API 추가

--- Week05/Day20_Assignment/Day20_Assignment/NetworkManager.swift
+++ Week05/Day20_Assignment/Day20_Assignment/NetworkManager.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import Alamofire
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func callRequestPhoto(query: String, page: Int, color: String?, sort: String, completionHandler: @escaping (PhotoSearchResponse) -> Void) {
+        let url = "https://api.unsplash.com/search/photos"
+        let headers: HTTPHeaders = ["Authorization": "Client-ID \(APIKey.UNSPLASH_ACCESS)"]
+        var parameters: Parameters = [
+            "query": query,
+            "page": page,
+            "per_page": 20,
+            "order_by": sort
+        ]
+        
+        if let color = color {
+            parameters["color"] = color
+        }
+        
+        AF.request(url, method: .get, parameters: parameters, headers: headers)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: PhotoSearchResponse.self) { response in
+                switch response.result {
+                case .success(let value):
+                    completionHandler(value)
+                    
+                case .failure(let error):
+                    print(">>> 에러 발생 >>> 에러: \(error)")
+                }
+            }
+    }
+    
+    func callRequestStatistics(id: String, completionHandler: @escaping (PhotoStatistics) -> Void) {
+        let url = "https://api.unsplash.com/search/photos\(id)/statistics"
+        let headers: HTTPHeaders = ["Authorization": "Client-ID \(APIKey.UNSPLASH_ACCESS)"]
+        
+        AF.request(url, method: .get, headers: headers)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: PhotoStatistics.self) { response in
+                switch response.result {
+                case .success(let value):
+                    completionHandler(value)
+                case .failure(let error):
+                    print(">>> 에러 발생 >>> 에러: \(error)")
+                }
+            }
+    }
+    
+    
+
+}
