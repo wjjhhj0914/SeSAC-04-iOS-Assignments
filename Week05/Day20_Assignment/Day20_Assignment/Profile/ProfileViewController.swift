0d4fa912a5829a62e946265297df4457a7e9b4b6
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-02-03 23:29:21 +0900

    add: 프로필 화면 파일 생성 및 기본 구조 작성

---
+++ Week05/Day20_Assignment/Day20_Assignment/Profile/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewController.swift
+//  Day20_Assignment
+//
+//  Created by Hyojung Jang on 2/3/26.
+//
+
+import UIKit
+
+final class ProfileViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func configureHierarchy() {
+        <#code#>
+    }
+    
+    override func configureLayout() {
+        <#code#>
+    }
+    
+    override func configureView() {
+        <#code#>
+    }
+}
