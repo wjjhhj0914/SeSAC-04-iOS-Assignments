c75e91eab0218e8629c5389871ab6f5b66adbce6
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-02-10 17:07:57 +0900

    chore: 프로젝트 생성

---
+++ Week07/Day30_Assignment_MVVM/Day30_Assignment_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day30_Assignment_MVVM
+//
+//  Created by Hyojung Jang on 2/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
