3ec7f577379eaa0750b0d8c450820779507a0e5d
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-02-10 21:25:39 +0900

    [BMICalculate] 로직 분리 및 데이터 입출력 변수 정리

---
+++ Week07/Day30_Assignment_MVVM/Day30_Assignment_MVVM/BMICalculate/BMICalculateModel.swift
@@ -0,0 +1,74 @@
+//
+//  BMICalculateModel.swift
+//  Day30_Assignment_MVVM
+//
+//  Created by Hyojung Jang on 2/10/26.
+//
+
+import Foundation
+
+class BMICalculateModel {
+    // 처음에는 실행 X
+    var heightInputField = "" {
+        didSet {
+            print("heightInputField", heightInputField)
+        }
+    }
+    
+    var weightInputField = "" {
+        didSet {
+            print("weightInputField", weightInputField)
+        }
+    }
+    
+    var outputText = "" {
+        didSet {
+            print("outputText", outputText)
+        }
+    }
+    
+    private func calculateBmi() {
+        
+        // 1) 빈 값
+        guard !heightInputField.isEmpty, !weightInputField.isEmpty else {
+            outputText = "키와 몸무게를 입력해주세요"
+//            self.resultLabel.backgroundColor = .red.withAlphaComponent(0.3)
+            return
+        }
+        
+        // 2) 숫자 변환
+        guard let height = Double(heightInputField),
+              let weight = Double(weightInputField) else {
+            outputText = "숫자만 입력해주세요"
+//            self.resultLabel.backgroundColor = .red.withAlphaComponent(0.3)
+            return
+        }
+        
+        // 3) 유효 범위
+        guard height > 0, weight > 0 else {
+            outputText = "0보다 큰 값을 입력해주세요"
+//            self.resultLabel.backgroundColor = .red.withAlphaComponent(0.3)
+            return
+        }
+        
+        // 4) BMI 계산
+        let heightInMeter = height / 100
+        let bmi = weight / (heightInMeter * heightInMeter)
+        let bmiText = String(format: "%.1f", bmi)
+        
+        switch bmi {
+        case ..<18.5:
+            outputText = "BMI: \(bmiText) (저체중)"
+//            self.resultLabel.backgroundColor = .yellow.withAlphaComponent(0.3)
+        case 18.5..<25:
+            outputText = "BMI: \(bmiText) (정상) ✅"
+//            self.resultLabel.backgroundColor = .green.withAlphaComponent(0.3)
+        case 25..<30:
+            outputText = "BMI: \(bmiText) (과체중)"
+//            self.resultLabel.backgroundColor = .yellow.withAlphaComponent(0.3)
+        default:
+            outputText = "BMI: \(bmiText) (비만)"
+//            self.resultLabel.backgroundColor = .yellow.withAlphaComponent(0.3)
+        }
+    }
+}
--- Week07/Day30_Assignment_MVVM/Day30_Assignment_MVVM/BMICalculate/BMICalculateViewController.swift
+++ Week07/Day30_Assignment_MVVM/Day30_Assignment_MVVM/BMICalculate/BMICalculateViewController.swift
@@ -34,6 +34,8 @@ class BMIViewController: UIViewController {
         return label
     }()
     
+    let viewModel = BMICalculateModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
@@ -52,52 +54,8 @@ class BMIViewController: UIViewController {
     }
     
     private func calculate() {
-        guard let heightText = heightTextField.text, let weightText = weightTextField.text else {
-            return
-            
-        }
-        
-        // 1) 빈 값
-        guard !heightText.isEmpty, !weightText.isEmpty else {
-            self.resultLabel.text = "키와 몸무게를 입력해주세요"
-            self.resultLabel.backgroundColor = .red.withAlphaComponent(0.3)
-            return
-        }
-        
-        // 2) 숫자 변환
-        guard let height = Double(heightText),
-              let weight = Double(weightText) else {
-            self.resultLabel.text = "숫자만 입력해주세요"
-            self.resultLabel.backgroundColor = .red.withAlphaComponent(0.3)
-            return
-        }
-        
-        // 3) 유효 범위
-        guard height > 0, weight > 0 else {
-            self.resultLabel.text = "0보다 큰 값을 입력해주세요"
-            self.resultLabel.backgroundColor = .red.withAlphaComponent(0.3)
-            return
-        }
-        
-        // 4) BMI 계산
-        let heightInMeter = height / 100
-        let bmi = weight / (heightInMeter * heightInMeter)
-        let bmiText = String(format: "%.1f", bmi)
-        
-        switch bmi {
-        case ..<18.5:
-            self.resultLabel.text = "BMI: \(bmiText) (저체중)"
-            self.resultLabel.backgroundColor = .yellow.withAlphaComponent(0.3)
-        case 18.5..<25:
-            self.resultLabel.text = "BMI: \(bmiText) (정상) ✅"
-            self.resultLabel.backgroundColor = .green.withAlphaComponent(0.3)
-        case 25..<30:
-            self.resultLabel.text = "BMI: \(bmiText) (과체중)"
-            self.resultLabel.backgroundColor = .yellow.withAlphaComponent(0.3)
-        default:
-            self.resultLabel.text = "BMI: \(bmiText) (비만)"
-            self.resultLabel.backgroundColor = .yellow.withAlphaComponent(0.3)
-        }
+        viewModel.heightInputField = heightTextField.text!
+        viewModel.weightInputField = weightTextField.text!
     }
 }
 
