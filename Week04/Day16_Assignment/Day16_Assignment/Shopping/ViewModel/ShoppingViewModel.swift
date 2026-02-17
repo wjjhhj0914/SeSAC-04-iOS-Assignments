5805906985aa0494faa2cd9f58ae5afe314e0734
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-02-17 16:21:30 +0900

    feat: ShoppingViewModel 구현 및 검색어 유효성 검사 로직 이전

--- Week04/Day16_Assignment/Day16_Assignment/Shopping/ViewModel/ShoppingViewModel.swift
+++ Week04/Day16_Assignment/Day16_Assignment/Shopping/ViewModel/ShoppingViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+final class ShoppingViewModel {
+    
+    let inputViewDidLoadTrigger = Observable(())
+    let inputSearchKeyword = Observable("")
+    let inputSearchButtonClicked = Observable(())
+    
+    let outputSearchHistory: Observable<[String]> = Observable([])
+    let outputValidSearchKeyword = Observable<String?>(nil)
+    let outputAlertMessage = Observable<String?>(nil)
+    
+    init () {
+        print("ShoppingViewModel Init")
+        
+        inputViewDidLoadTrigger.bind {
+            print("inputViewDidLoadTrigger 신호 받음")
+            self.outputSearchHistory.value = UserDefaultManager.searchHistory
+        }
+        
+        inputSearchButtonClicked.bind {
+            print("inputSearchButtonClicked")
+            self.search()
+        }
+    }
+    
+    private func search() {
+        let userInputText = inputSearchKeyword.value
+        
+        if userInputText.count < 2 {
+            outputAlertMessage.value = "2글자 이상 입력해주세요"
+        } else {
+            outputValidSearchKeyword.value = userInputText
+        }
+    }
+}
