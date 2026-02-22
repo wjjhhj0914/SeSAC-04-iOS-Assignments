e63cca247c0f537d3b097f759e9cee6ede371f6e
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-02-22 21:28:58 +0900

    feat: NumbersViewController 실시간 값 변화 레이블에 출력

--- Week08/RxSampleProject/RxSampleProject/Numbers/NumbersViewController.swift
+++ Week08/RxSampleProject/RxSampleProject/Numbers/NumbersViewController.swift
@@ -6,24 +6,83 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
 
 class NumbersViewController: BaseViewController {
+    
+    private let number1 = UITextField()
+    private let number2 = UITextField()
+    private let number3 = UITextField()
+    private let plusLabel = UILabel()
+    private let separator = UIView()
+    private let result = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    override func configureHierarchy() {
+        [number1, number2, number3, result, plusLabel, separator].forEach { view.addSubview($0) }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func configureLayout() {
+        number1.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.centerY.equalTo(view.safeAreaLayoutGuide)
+            make.width.equalTo(150)
+        }
+        
+        number2.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(number1.snp.bottom).offset(8)
+            make.width.equalTo(150)
+        }
+        
+        number3.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(number2.snp.bottom).offset(8)
+            make.width.equalTo(150)
+        }
+        
+        plusLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(number3)
+            make.trailing.equalTo(number3.snp.leading).offset(-8)
+        }
+        
+        separator.snp.makeConstraints { make in
+            make.top.equalTo(number3.snp.bottom).offset(8)
+            make.leading.equalTo(plusLabel.snp.leading)
+            make.trailing.equalTo(number3.snp.trailing)
+            make.height.equalTo(1)
+        }
+        
+        result.snp.makeConstraints { make in
+            make.top.equalTo(separator.snp.bottom).offset(8)
+            make.trailing.equalTo(separator.snp.trailing)
+            make.width.equalTo(150)
+        }
     }
-    */
-
+    
+    override func configureView() {
+        [number1, number2, number3].forEach {
+            $0.placeholder = "number"
+            $0.textAlignment = .right
+            $0.keyboardType = .numberPad
+        }
+        
+        plusLabel.text = "+"
+        separator.backgroundColor = .lightGray
+        result.textAlignment = .right
+    }
+    
+    override func bind() {
+        Observable.combineLatest(number1.rx.text.orEmpty, number2.rx.text.orEmpty, number3.rx.text.orEmpty) { textValue1, textValue2, textValue3 -> Int in
+            return (Int(textValue1) ?? 0) + (Int(textValue2) ?? 0) + (Int(textValue3) ?? 0)
+        }
+        .map(\.description)
+        .bind(to: result.rx.text)
+        .disposed(by: disposeBag)
+    }
 }
