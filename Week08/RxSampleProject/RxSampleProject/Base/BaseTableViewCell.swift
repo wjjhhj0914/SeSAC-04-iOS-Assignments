a1eaf0da9d3a3e0e2677dac6cdb7b030932d0d22
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-02-22 19:38:42 +0900

    feat: BaseTableViewCell 생성

---
+++ Week08/RxSampleProject/RxSampleProject/Base/BaseTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  BaseTableViewCell.swift
+//  RxSampleProject
+//
+//  Created by Hyojung Jang on 2/22/26.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() {}
+    func configureLayout() {}
+    func configureView() {}
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
