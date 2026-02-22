89f92e46cee7a73354d51bfd6e2f6884c8128d68
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-02-22 22:50:08 +0900

    feat: ValidationViewController 유효성 검사 구현

--- Week08/RxSampleProject/RxSampleProject/Validation/ValidationViewController.swift
+++ Week08/RxSampleProject/RxSampleProject/Validation/ValidationViewController.swift
@@ -6,24 +6,148 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
+
+private let minimalUsernameLength = 5
+private let minimalPasswordLength = 5
 
 class ValidationViewController: BaseViewController {
+    
+    private let usernameTitleLabel = {
+        let label = UILabel()
+        label.text = "Username"
+        return label
+    }()
+    
+    private let usernameTextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let usernameValidLabel = {
+        let label = UILabel()
+        label.textColor = .systemRed
+        label.text = "아이디는 최소 5자 이상이어야 합니다"
+        return label
+    }()
+    
+    private let passwordTitleLabel = {
+        let label = UILabel()
+        label.text = "Password"
+        return label
+    }()
+    
+    private let passwordTextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    private let passwordValidLabel = {
+        let label = UILabel()
+        label.textColor = .systemRed
+        label.text = "비밀번호는 최소 5자 이상이어야 합니다"
+        return label
+    }()
+    
+    private let doSomethingButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemGreen
+        button.setTitle("Do something", for: .normal)
+        button.layer.cornerRadius = 8
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    override func configureHierarchy() {
+        [usernameTitleLabel, usernameTextField, usernameValidLabel, passwordTitleLabel, passwordTextField, passwordValidLabel, doSomethingButton].forEach { view.addSubview($0) }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func configureLayout() {
+        usernameTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        usernameTextField.snp.makeConstraints { make in
+            make.top.equalTo(usernameTitleLabel.snp.bottom).offset(8)
+            make.leading.trailing.equalTo(usernameTitleLabel)
+            make.height.equalTo(44)
+        }
+        
+        usernameValidLabel.snp.makeConstraints { make in
+            make.top.equalTo(usernameTextField.snp.bottom).offset(8)
+            make.leading.trailing.equalTo(usernameTitleLabel)
+        }
+        
+        passwordTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(usernameValidLabel.snp.bottom).offset(8)
+            make.leading.trailing.equalTo(usernameTitleLabel)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(passwordTitleLabel.snp.bottom).offset(8)
+            make.leading.trailing.equalTo(usernameTitleLabel)
+            make.height.equalTo(44)
+        }
+        
+        passwordValidLabel.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(8)
+            make.leading.trailing.equalTo(usernameTitleLabel)
+        }
+        
+        doSomethingButton.snp.makeConstraints { make in
+            make.top.equalTo(passwordValidLabel.snp.bottom).offset(24)
+            make.leading.trailing.equalTo(usernameTitleLabel)
+            make.height.equalTo(44)
+        }
     }
-    */
-
+    
+    override func bind() {
+        let usernameValid = usernameTextField.rx.text.orEmpty
+            .map { $0.count >= minimalUsernameLength }
+            .share(replay: 1)
+        
+        let passwordValid = passwordTextField.rx.text.orEmpty
+            .map { $0.count >= minimalPasswordLength }
+            .share(replay: 1)
+        
+        let everythingValid = Observable.combineLatest(usernameValid, passwordValid) { $0 && $1 }
+            .share(replay: 1)
+        
+        usernameValid
+            .bind(to: passwordTextField.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        usernameValid
+            .bind(to: usernameValidLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        passwordValid
+            .bind(to: passwordValidLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        everythingValid
+            .bind(to: doSomethingButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        doSomethingButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in self?.alert(title: "알림", message: "환영합니다!") })
+            .disposed(by: disposeBag)
+    }
+    
+    private func alert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(ok)
+        self.present(alert, animated: true)
+    }
 }
